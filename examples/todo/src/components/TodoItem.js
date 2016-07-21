6d173fb3bd2f44a2124f64dbd4d726201fd6c50f
Wei Zhu <yesmeck@gmail.com>
2016-07-21 11:52:12 +0800

    Declare styles directly

--- examples/todo/src/components/TodoItem.js
+++ examples/todo/src/components/TodoItem.js
@@ -2,16 +2,13 @@ import React from 'react'
 import { StyleSheet, css } from 'aphrodite'
 import { ListItem, Checkbox } from 'material-ui'
 
-function getStyles() {
-  return StyleSheet.create({
-    completed: {
-      textDecoration: "line-through",
-    }
-  })
-}
+const styles = StyleSheet.create({
+  completed: {
+    textDecoration: "line-through",
+  }
+})
 
 export default function TodoItem({ todo, handleCheck }) {
-  const styles = getStyles()
   const checkbox = <Checkbox checked={todo.completed} onCheck={handleCheck(todo)} />
 
   return (
--- examples/todo/src/containers/App.js
+++ examples/todo/src/containers/App.js
@@ -3,18 +3,14 @@ import { StyleSheet, css } from 'aphrodite'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import { AppBar } from 'material-ui'
 
-function getStyles() {
-  return StyleSheet.create({
-    root: {
-      width: "380px",
-      margin: "100px auto 0 auto",
-    }
-  })
-}
+const styles = StyleSheet.create({
+  root: {
+    width: "380px",
+    margin: "100px auto 0 auto",
+  }
+})
 
 export default function App(props) {
-  const styles = getStyles()
-
   return (
     <MuiThemeProvider>
       <div className={css(styles.root)}>
