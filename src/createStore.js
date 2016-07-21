95f311dfe6b950177831e5eeb4d1eb7fc13c31c9
Wei Zhu <yesmeck@gmail.com>
2016-07-21 16:32:48 +0800

    support nested namespace

--- src/createStore.js
+++ src/createStore.js
@@ -1,12 +1,23 @@
 import { createStore as _createStore, applyMiddleware, combineReducers } from 'redux'
+import set from 'lodash/set'
 
 function createStore(models, middlewares, initialState) {
   const reducers = models.reduce((acc, model) => {
-    acc[model.getNamespace()] = model.getReducer()
+    const nsPath = model.getNamespace().replace('::', '.')
+    set(acc, nsPath, model.getReducer())
     return acc
   }, {})
 
-  const reducer = combineReducers(reducers)
+  const combine = (reducers) => {
+    Object.keys(reducers).map(key => {
+      if (typeof reducers[key] === 'object') {
+        reducers[key] = combineReducers(reducers[key])
+      }
+    })
+    return combineReducers(reducers)
+  }
+
+  const reducer = combine(reducers)
 
   const enhancer = middlewares && middlewares.length > 0 ? applyMiddleware(...middlewares) : undefined
 
--- test/createStore.spec.js
+++ test/createStore.spec.js
@@ -20,3 +20,51 @@ test('create store', t => {
 
   t.is(store.getState().counter, 1)
 })
+
+test('nested namespace', t => {
+  const counterFactory = ({ namespace, state }) => {
+    const counter = model({
+      namespace,
+      state,
+    })
+
+    counter.action('increment')
+
+    counter.reducer(on => {
+      on(counter.increment, state => state + 1)
+    })
+
+    return counter
+  }
+
+  const counterOne = counterFactory({
+    namespace: 'counter::one',
+    state: 1
+  })
+
+  const counterTwo = counterFactory({
+    namespace: 'counter::two',
+    state: 2
+  })
+
+  const store = createStore([counterOne, counterTwo])
+
+  t.deepEqual(store.getState().counter, {
+    one: 1,
+    two: 2,
+  })
+
+  store.dispatch(counterOne.increment())
+
+  t.deepEqual(store.getState().counter, {
+    one: 2,
+    two: 2,
+  })
+
+  store.dispatch(counterTwo.increment())
+
+  t.deepEqual(store.getState().counter, {
+    one: 2,
+    two: 3,
+  })
+})
